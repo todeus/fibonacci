5a3160090832f310827099ded3bea2a45b97d55d
todeus <gtodeus@gmail.com>
2010-03-18 06:00:28 -0700

    Оптимизированно под матлаб.

--- fibonacci.m
+++ fibonacci.m
@@ -9,17 +9,17 @@ n=0;
 
 while(fn3<d)
   fn1=fn3;
-  fn2+=fn3;
-  fn3+=fn2;
+  fn2=fn2+fn3;
+  fn3=fn3+fn2;
   n=n+1;
-endwhile;
-n;
+end;
+
 x1=a+(b-a)*fn1/fn3;
 x2=a+(b-a)*fn2/fn3;
 y1=sin(2*x1);
 y2=sin(2*x2);
 
-while n!=1
+while not (n==1)
     if y1<y2
       	a=x1;
 	x1=x2;
@@ -32,14 +32,14 @@ while n!=1
 	x1=a+(b-x2);
         y2=y1;
 	y1=sin(2*x1);
-    endif;
-    n--;
-endwhile;
+    end;
+    n=n-1;
+end;
 
 if y1>=y2
     alpha=x1;
 else
     alpha=x2;
-endif;
+end;
 alpha
 sin(2*alpha)
